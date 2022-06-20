d6a7e973e0fe6db7aaa67fdac0efec84533b7511
Jasper <jjcharlinski@gmail.com>
2022-06-19 22:00:15 -0700

    Email script added, EmailJS server linked.

---
+++ script/emailScript.js
@@ -0,0 +1,38 @@
+// JJWebDesign
+// * /  .\   *
+// * /  =__| * 
+// * /    || *
+// * * * * * *
+
+// Initialize connection with public key
+(function(){ emailjs.init("DknL__q9iYDp74hMS"); })();
+
+document.getElementById(`sendMessageButton`).addEventListener(`click`, function () {
+
+    const name = document.getElementById(`nameInput`).value;
+    const fromEmail = document.getElementById(`emailInput`).value;
+    const fromPhone = document.getElementById(`phoneInput`).value;
+    const message  = document.getElementById(`inquiryTextbox`).value;
+
+    if ((name == ``) || (fromEmail == ``) || (fromPhone == ``))
+    {
+        alert(`Contact information can not be left blank.`);
+        return;
+    }
+
+    if (message == ``)
+    {
+        message = `No message sent.`;
+    }
+
+    let contents = {
+
+        from_name: name,
+        email: fromEmail,
+        phone: fromPhone,
+        message: message
+    };
+
+    emailjs.send(`service_l64lhbp`, `template_g4j1up5`, contents).then(function() {alert(`Message Sent.`)});
+
+});
